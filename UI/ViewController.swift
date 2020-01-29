84556ad340461b0146e8c8f76f7e9f2645350afe
Jaehun Jung <jjhisking@gamil.com>
2020-01-29 14:40:55 +0900

    Initial Commit

---
+++ UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI
+//
+//  Created by 정재훈 on 2020/01/29.
+//  Copyright © 2020 littlemenu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
