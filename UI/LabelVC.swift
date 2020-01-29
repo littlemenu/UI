a3f79f6fe581109be11d92da950d906846dd783e
Jaehun Jung <jjhisking@gamil.com>
2020-01-29 14:48:03 +0900

    Create Label View Controller

---
+++ UI/LabelVC.swift
@@ -0,0 +1,30 @@
+//
+//  LabelVC.swift
+//  UI
+//
+//  Created by 정재훈 on 2020/01/29.
+//  Copyright © 2020 littlemenu. All rights reserved.
+//
+
+import UIKit
+
+class LabelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
